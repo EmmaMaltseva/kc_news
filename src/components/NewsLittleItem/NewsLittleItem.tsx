a8653203008a986d8ef80598fddc83c8f2d8f7a6
EmmaMaltseva <emmamaltseva0@gmail.com>
2024-05-29 11:54:24 +0300

    добавила адаптив

--- src/components/NewsLittleItem/NewsLittleItem.tsx
+++ src/components/NewsLittleItem/NewsLittleItem.tsx
@@ -9,16 +9,28 @@ interface NewsItemProps {
 
 const NewsItem: FC<NewsItemProps> = ({news, onClick}) => {
     return (
-        <div onClick={() => onClick(news)} className="little-item-news">
-            <div>
+        <div onClick={() => onClick(news)} className="little-item-news btn-new">
+            <div className="box-image little-item-image">
+                <img
+                    className="image-little-item-news"
+                    src={news.image}
+                    alt="Изображение к новости"
+                    onError={({currentTarget}) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src = 'https://tsul.uz/images/default-news-image.png';
+                    }}
+                />
+            </div>
+            <div className="little-news-description-body">
+                <p className="news-little-category">{news.category_id.toUpperCase()}</p>
                 <h3 className="little-news-title">{news.title}</h3>
+                <p className="news-little-description">{news.description}</p>
                 <div className="little-news-description">
                     <p className="little-news-date">{news.date.slice(4, 10)}</p>
                     <p className="little-news-source">
                         <a href='/' className="little-news-source-link">ИСТОЧНИК</a>
                     </p>
                 </div>
-
             </div>
         </div>
     );
